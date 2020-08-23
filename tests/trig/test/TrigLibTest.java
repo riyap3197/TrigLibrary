2dfe8f45d66a9a4cd5d6ff2007962c94e7db323f
Riya Hemant Patel <patel79@uwindsor.ca>
2020-08-23 13:08:39 -0400

    Trigonometric functions
    Testing the trigonometric functions

---
+++ tests/trig/test/TrigLibTest.java
@@ -0,0 +1,35 @@
+package trig.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TrigLibTest {
+
+	@Test
+	void sinTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void cosTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void tanTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void toDegreesTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void toRadiansTest() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
